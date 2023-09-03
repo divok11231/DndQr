f42a481c2aceca4e8e425f928c0c9243ab89e0cf
divok11231 <lkovid@gmail.com>
2023-09-03 23:07:30 +0530

    feat: whatever i said before im retarded

---
+++ src/components/redText .tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { supabase } from '../lib/supabase.client';
+import "../layouts/green-page-style.css";
+
+function NiceMessage() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetchMessage();
+  }, []);
+
+  async function fetchMessage() {
+    // get a random row from the DndQr table
+    const { data, error } = await supabase
+      .from("currentPageUrl")
+      .select("goneMessage")
+      .eq('currentPageUrl', 'nignognaggins')
+    if (error) {
+      console.error(error);
+    } else {
+      // set the message state to the nice_message value
+      setMessage(data[0].goneMessage);
+    }
+  }
+
+  return (
+    <p className="text-wrapper">
+      <h1>{message}</h1>
+    </p>
+  );
+}
+
+export default NiceMessage;
